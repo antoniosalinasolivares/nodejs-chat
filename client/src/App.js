de062171ca85921d74a984cf45494241d2ee1e82
antoniosalinasolivares <antonio.salinas1998@comunidad.unam.mx>
2021-02-15 11:39:17 -0600

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Home } from './components/Home/Home';
 import { Chat } from './components/Chat/Chat';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {UserContext} from './UserContext'
+import {Navbar} from './components/layout/Navbar';
 const App = () => {
 
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({name:'Tony'});
 
 
   return (
     <Router>
       <UserContext.Provider value={{user, setUser}}>
+        <Navbar/>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/chat' component={Chat} />
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,38 @@
+const Navbar = () => {
+    return (
+        <>
+            <nav className="green">
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Logo</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="sass.html">Login</a></li>
+                        <li><a href="badges.html">Signup</a></li>
+                        <li><a href="collapsible.html">Logout</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#!" class="brand-logo">Logo</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                </ul>
+                </div>
+            </nav>
+
+            <ul class="sidenav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+        </>
+    )
+}
+
+export {Navbar}
